679346f75d0c0b8cde3f390a0efa26378d12c7db
Justin Blinder <justin.blinder@gmail.com>
2013-06-11 22:08:41 -0400

    Speed project commit. All teh files

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-DarkSideOfThePrism
-==================
+![img](http://www.justinblinder.com/images/prismlogo.jpg)
 
-DarkSideOfThePrism
+A **Firefox Add-on** that provides a soundtrack for our surveilled internet meanderings.
+
+"Dark Side of the Prism" uses Pink Floyd's aural prism (Dark Side of the Moon) as a playlist to the NSA's tracking efforts, serving as an auditory reminder of how our online activities are surveilled.
+
+#Installing
+Double click on the darksideoftheprism.xpi file and it should install on your Firefox browser. 
+
+#Use
+When visiting any URLs that have been (publicly) targeted by Prism, a small notification will appear alerting that this site is being surveilled. You will then be presented with an ethereal Pink Floyd track from the "Dark Side of the Moon" album.
+
+#Notes
+- The Pink Floyd tracks come from http://www.floydhead.com/MIDI.htm
+
+- There is no Google Chrome extension. Instead, listen to the entire "Dark Side of the Moon" album on reapeat while you surf the internet.
+
+![img](http://www.justinblinder.com/images/aspeed.jpg)
---
+++ data/prism.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+	var clips = ["darkside.ogg","brain.ogg","money.ogg", "breath.ogg", "eclipse.ogg", "gig.ogg", "ontherun.ogg","speaktome.ogg","time.ogg", "usandthem.ogg"];   
+	// Basic list of providers based on media coverage
+	var prismSitez = ["google.com", "gmail.com", "facebook.com", "bing.com", "yahoo.com", "aol.com","apple.com","skype.com", "microsoft.com", "youtube.com","hotmail.com"];
+
+	var isPlaying = false;	
+	var currentSite = 0;
+	var isWeWatched = false;
+
+	for (var i = 0; i < prismSitez.length; i++) {
+  		if(window.location.host.indexOf(prismSitez[i]) > -1) {
+  			console.log("sitecheck: " + window.location.host);
+ 			isWeWatched = true;
+ 			currentSite = i;
+ 			console.log(prismSitez[i]);
+ 		}	
+	}
+	if ( isWeWatched === true && isPlaying === false) { 
+		var b = document.createElement('audio');
+		if ( currentSite > clips.length ) currentSite = clips.length;
+		b.src = "http://66.228.34.242/audio/aud/"+ clips[currentSite];
+		b.play();
+		isPlaying = true; // Content script getting loaded twice on gamil.com <- WTF?
+		document.body.innerHTML += '<div style="position:absolute; top:-500px; left:80%;" id="prism-prism"><img src="http://66.228.34.242/audio/picon.jpg" style="width:75%; heigh:75%;"/></div>';	
+		function hide(){
+			 $('#prism-prism').animate({ opacity: 0}, 6000);
+		}
+		setTimeout(function(){
+	 		$('#prism-prism').css('z-index', 9999); 
+			$('#prism-prism').animate({top: '10px'}, 1500, function(){ hide();});
+		}, 100);
+	}
+});
+
+	
+	
+	
+	
+	
+	
+	
+	
